e56ffd25994c91b073bdf8526c0205226f3f6952
Joshua Bell <belljh@gmail.com>
2022-07-24 15:29:53 -0700

    updated to include index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.use('/css',express.static(__dirname +'/public/css'));
+app.use('/js',express.static(__dirname +'/public/js'));
+
+app.get('/', (req, res) => {
+    // res.send('Autographer')
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
